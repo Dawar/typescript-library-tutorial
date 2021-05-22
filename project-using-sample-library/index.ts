cf3cdf6e0cf058ebe2378ccc7ba7bf25bed3dd5c
Dawar Rashid <dr@dawar.ca>
2021-05-22 13:59:59 -0400

    project implementatino

---
+++ project-using-sample-library/index.ts
@@ -0,0 +1,12 @@
+// Here we import a common module from the root exports
+import { timeout } from 'sample-typescript-library';
+// Here we import node-specific code
+import { getPackage } from 'sample-typescript-library/node';
+
+async function main() {
+	console.log('logging package in 5...');
+	await timeout(5000);
+	console.log(getPackage());
+}
+
+main();
